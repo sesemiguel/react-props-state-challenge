37e6cbbe4e95c1324be9e98252188dcd9971c559
Miguel Karlo Sese <sese.miguel9@gmail.com>
2022-11-13 16:53:35 +0800

    Update Challenge4.js

--- react-props-state-challenge/src/components/Challenge4.js
+++ react-props-state-challenge/src/components/Challenge4.js
@@ -3,14 +3,11 @@ import React from 'react';
 export default function Challenge4(props){
 
     const [thingsArray, setThingsArray] = React.useState(['Thing 1', 'Thing 2'])
-
-    const addThing = () => setThingsArray(prevThingsArray => [...prevThingsArray, `Thing ${prevThingsArray.length+1}`]);
-    
     const thingsArrayElements = thingsArray.map(thing => <p key={thing}>{thing}</p>)
 
     return(
         <>
-            <button onClick={addThing}>Add Item</button>
+            <button onClick={() => setThingsArray(prevThingsArray => [...prevThingsArray, `Thing ${prevThingsArray.length+1}`])}>Add Item</button>
             {thingsArrayElements}
         </>
     );
